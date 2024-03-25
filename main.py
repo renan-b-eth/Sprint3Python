841e7e4dc234de2e8616b43e4a0d91748916335d
renan-b-eth <renanacademic21@gmail.com>
2024-03-25 19:47:53 -0300

    estrutura basica da sprint3 python

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as ny
+import matplotlib.pyplot as plt
+
+# realizar a % de tempo que o usuario clica na tela e quantos clicks.
+class Main():
+
+    def ler_csv(arquivo):
+        df = pd.read_csv("./Sprint3Python/dados/")
+        return df
+    
+    def porcentagem(df):
+        return "opa"
+    
+    def qtd_clicks(df):
+        return "opa"
+    
+
